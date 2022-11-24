6c7f8d818ce961452d9f49d5844b030162cad280
Mathis <mathis.levieux1@gmail.com>
2022-11-24 18:34:55 +0100

    maj

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-const inputmodel = document.querySelector("#inputmodel")
+const inputList = document.querySelector("#inputlist")
+const inputModel = document.querySelector("#inputmodel")
+const addGuestButton = document.querySelector("#addguest")
+const delAllGuestButton = document.querySelector("#delallguest")
+const delGuestButton = document.querySelector("#delguest")
+
+// Ajoute un invité à chaque clic
+
+addGuestButton.addEventListener("click", () => {
+    inputList.appendChild(inputModel.cloneNode(true));
+})
+
+
+
+// Supprime toute la liste, puis recrée un invité
+
+delAllGuestButton.addEventListener("click", () => {
+    inputList.innerHTML = ""
+    inputList.appendChild(inputModel.cloneNode(true));
+})
+
+
+// Supprime l'invité
+
+// delGuestButton.addEventListener("click", () => {
+//     inputModel.parentNode.removeChild(inputModel);
+// })
