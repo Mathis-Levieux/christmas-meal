e17215a9eb256bb6fef643b61a2d5d8dcd383117
Mathis Levieux <mathis.levieux1@gmail.com>
2022-11-25 10:15:18 +0100

    fonctionne mais ne comprend pas pourquoi

--- script.js
+++ script.js
@@ -3,11 +3,19 @@ const inputModel = document.querySelector("#inputmodel")
 const addGuestButton = document.querySelector("#addguest")
 const delAllGuestButton = document.querySelector("#delallguest")
 const delGuestButton = document.querySelector(".delguest")
+const inputFirstName = document.getElementById("firstname")
+const inputLastName = document.getElementById("lastname")
+
+
+const inputModelTest = `<div id="inputlist"><div id="inputmodel"><input placeholder="Nom" type="text" id="lastname" name="name">
+    <input placeholder="Prénom" type="text" id="firstname" name="name">
+    <img id="delguest" class="delguest" src="/icons8-signe-moins-18.png"></div>`
+inputList.innerHTML = inputModelTest
 
 // Ajoute un invité à chaque clic
 
-addGuestButton.addEventListener("click", () => {
-    inputList.appendChild(inputModel.cloneNode(true));
+addGuestButton.addEventListener("click", event => {
+    inputList.appendChild(inputModelTest.cloneNode(true));
 })
 
 
