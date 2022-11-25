9dcf29eada60dd8a860b017850ccdbc3f14d5a8a
Mathis Levieux <mathis.levieux1@gmail.com>
2022-11-25 10:43:37 +0100

    Avancées

--- script.js
+++ script.js
@@ -5,28 +5,23 @@ const delAllGuestButton = document.querySelector("#delallguest")
 const delGuestButton = document.querySelector(".delguest")
 const inputFirstName = document.getElementById("firstname")
 const inputLastName = document.getElementById("lastname")
-
-
 const inputModelTest = `<div id="inputlist"><div id="inputmodel"><input placeholder="Nom" type="text" id="lastname" name="name">
     <input placeholder="Prénom" type="text" id="firstname" name="name">
     <img id="delguest" class="delguest" src="/icons8-signe-moins-18.png"></div>`
 inputList.innerHTML = inputModelTest
 
-// Ajoute un invité à chaque clic
+const sendButton = document.querySelector("#send")
 
+// Ajoute un invité à chaque clic
 addGuestButton.addEventListener("click", event => {
-    inputList.appendChild(inputModelTest.cloneNode(true));
+    inputList.appendChild(inputModel.cloneNode(true));
 })
 
-
-
 // Supprime toute la liste, puis recrée un invité
-
 delAllGuestButton.addEventListener("click", () => {
     inputList.innerHTML = ""
     inputList.appendChild(inputModel.cloneNode(true));
 })
-
 
 // Supprime l'invité choisi
 
